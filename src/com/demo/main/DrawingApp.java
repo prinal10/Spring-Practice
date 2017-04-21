555040cae34c2d6ddfadf1158a04d55b2f754297
Prinalchandra Patel <sudhirronaldo07@gmail.com>
2017-04-21 13:44:25 -0400

    init and destroy

--- src/com/demo/main/DrawingApp.java
+++ src/com/demo/main/DrawingApp.java
@@ -1,6 +1,6 @@
 package com.demo.main;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.demo.model.Triangle;
@@ -18,7 +18,19 @@ public class DrawingApp {
 		// Application context is similar to the bean factory but it offers more
 		// function like event notification, aop, etc. But requires more
 		// resources then bean factory. we do'nt have to instantiate file stream
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext("spring.xml");
+
+		/*
+		 * This is similar to ApplicationContext but used only we are developing
+		 * a SE application i.e a desktop application where we have to
+		 * explicitly have to destroy context, this can be done by calling the
+		 * registerShutdownHook() method which will destroy the context at the
+		 * end of the program. But in the web application the application
+		 * context will know when to destroy the context.
+		 */
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 
 		// instead of explicitly creating a new object we used bean factory to
 		// use xml file and create a object in the spring bean container using
--- src/com/demo/model/Triangle.java
+++ src/com/demo/model/Triangle.java
@@ -2,7 +2,12 @@ package com.demo.model;
 
 import java.util.List;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
 
 	/*
 	 * private String type; private int height;
@@ -33,10 +38,13 @@ public class Triangle {
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
-
 	private Point pointD;
-
+	private Point pointE;
 	private List<Point> points;
+
+	private ApplicationContext context = null;
+
+	private String beanName;
 
 	public List<Point> getPoints() {
 		return points;
@@ -78,12 +86,21 @@ public class Triangle {
 		this.pointD = pointD;
 	}
 
+	public Point getPointE() {
+		return pointE;
+	}
+
+	public void setPointE(Point pointE) {
+		this.pointE = pointE;
+	}
+
 	public void draw() {
 
 		System.out.println("This is point A---> X : " + getPointA().getX() + " & Y : " + getPointA().getY());
 		System.out.println("This is point B---> X : " + getPointB().getX() + " & Y : " + getPointB().getY());
 		System.out.println("This is point C---> X : " + getPointC().getX() + " & Y : " + getPointC().getY());
 		System.out.println("This is point D---> X : " + getPointD().getX() + " & Y : " + getPointD().getY());
+		System.out.println("This is point E---> X : " + getPointE().getX() + " & Y : " + getPointE().getY());
 
 		System.out.println("Loop begins from here------------------>");
 		for (Point point : points) {
@@ -92,6 +109,77 @@ public class Triangle {
 
 		}
 
+	}
+
+	/*
+	 * Whenever the spring bean i.e xml is initialized spring will see that the
+	 * bean class "Triangle" implements the ApplicationContextAware interface
+	 * and so will call the set below method "setApplicationContext", which we
+	 * can use to assign is to local context variable in the triangle class.
+	 * Using this context object we can initialize any member variable in the
+	 * triangle class even if the triangle bean in the xml file is with
+	 * singleton scope
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		// TODO Auto-generated method stub
+		this.context = context;
+
+	}
+
+	/*
+	 * Similarly when the spring bean sees that the triangle class has
+	 * implemented the BeanNameAware interface it will call the below method
+	 * setBeanName using which we can set the local memeber variable and use the
+	 * bean name anywhere
+	 */
+	@Override
+	public void setBeanName(String beanName) {
+		// TODO Auto-generated method stub
+		this.beanName = beanName;
+
+	}
+
+	/*
+	 * As we have implemented InitializingBean interface the spring bean will
+	 * call this method after the class (triangle bean)bean properties from the
+	 * xml has initialized.
+	 */
+	/*
+	 * @Override public void afterPropertiesSet() throws Exception { // TODO
+	 * Auto-generated method stub
+	 * 
+	 * System.out.println("After the initialization of Bean!!!");
+	 * 
+	 * }
+	 */
+	/*
+	 * As we have implemented DisposableBean interface the spring bean will call
+	 * this method before it destroys the bean(triangle bean)
+	 */
+	/*
+	 * @Override public void destroy() throws Exception { // TODO Auto-generated
+	 * method stub System.out.println("Before destroying the Bean!!!");
+	 * 
+	 * }
+	 */
+
+	/*
+	 * This is similar to the above afterPropertiesSet() method. This is
+	 * assigned as an initialization method in the spring xml file using
+	 * "init-method" para in the triangle bean tag
+	 */
+	public void myInit() {
+		System.out.println("After the initialization of Bean!!!");
+	}
+
+	/*
+	 * This is similar to the above destroy() method. This is assigned as an
+	 * destroy-method in the spring xml file using "destroy-method" para in the
+	 * triangle bean tag
+	 */
+	public void beforeDestroy() {
+		System.out.println("Before destroying the Bean!!!");
 	}
 
 }
